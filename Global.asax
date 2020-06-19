afdc9325bf84261146b09da9cb270e3ff723945d
FilBil <63282652+FilBil@users.noreply.github.com>
2020-06-19 10:59:40 +0200

    initialize

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StoreKom.MvcApplication" Language="C#" %>
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Informacje ogólne o zestawie zależą od poniższego
+// zestawu atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// związane z zestawem.
+[assembly: AssemblyTitle("StoreKom")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StoreKom")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie dla atrybutu ComVisible wartości false powoduje, że typy w tym zestawie stają się niewidoczne
+// dla składników COM. Jeśli musisz uzyskiwać dostęp do typu w tym zestawie
+// z modelu COM, ustaw dla atrybutu ComVisible tego typu wartość true.
+[assembly: ComVisible(false)]
+
+// Poniższy identyfikator GUID odpowiada atrybutowi ID biblioteki typów typelib, jeśli ten projekt jest uwidaczniany w modelu COM
+[assembly: Guid("d3c6590f-489a-469c-8cc9-cbe05ad92db0")]
+
+// Informacje o wersji zestawu obejmują następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie te wartości lub użyć wartości domyślnych numerów kompilacji i poprawki,
+// stosując znak „*”, jak pokazano poniżej:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
